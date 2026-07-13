a15e31053236d1600a926b2646fceec1951b9d8b
Tom Maple <tom.maple@outlook.com>
2026-07-13 08:46:43 -0400

    chore: Fixed comments

--- src/Maple.Json.ObjectAsPrimitiveConverter/Configuration/FloatFormat.cs
+++ src/Maple.Json.ObjectAsPrimitiveConverter/Configuration/FloatFormat.cs
@@ -6,20 +6,20 @@
 public enum FloatFormat
 {
     /// <summary>
-    ///     Use the [System.Decimal](https://learn.microsoft.com/en-us/dotnet/api/system.decimal) (`decimal`) to represent a
-    ///     floating-point number.
+    ///     Use the [System.Decimal](https://learn.microsoft.com/en-us/dotnet/api/system.decimal) (`decimal`)
+    ///     to represent a floating-point number.
     /// </summary>
     Decimal,
 
     /// <summary>
-    ///     Use the [System.Double](https://learn.microsoft.com/en-us/dotnet/api/system.double) (`double`) to represent a
-    ///     floating-point number.
+    ///     Use the [System.Double](https://learn.microsoft.com/en-us/dotnet/api/system.double) (`double`)
+    ///     to represent a floating-point number.
     /// </summary>
     Double,
 
     /// <summary>
-    ///     Use the [System.Single](https://learn.microsoft.com/en-us/dotnet/api/system.single) (`float`) to represent a
-    ///     floating-point number.
+    ///     Use the [System.Single](https://learn.microsoft.com/en-us/dotnet/api/system.single) (`float`)
+    ///     to represent a floating-point number.
     /// </summary>
     Single
 }
